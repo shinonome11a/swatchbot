0823b1e017daddfd0d0257d5db78eb0e5a56012f
shinonome11a <k.ikegami@cosylab.is.oit.ac.jp>
2020-12-15 01:43:01 +0900

    first commit

---
+++ initial.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo '% sudo apt install swatch'
+sudo apt install swatch
+echo '% sudo mkdir /etc/swatch'
+sudo mkdir /etc/swatch
+echo '% sudo cp -R ./* /etc/swatch'
+sudo cp -R ./* /etc/swatch
+echo "% echo '@reboot root /usr/bin/swatchdog -c /etc/swatch/conf.d/auth.log.conf -t /var/log/auth.log > /dev/null' | sudo tee /etc/cron.d/swatch"
+echo '@reboot root /usr/bin/swatchdog -c /etc/swatch/conf.d/auth.log.conf -t /var/log/auth.log > /dev/null' | sudo tee /etc/cron.d/swatch
---
+++ scripts/accessbot.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
+import urllib.request
+import os
+import socket
+import sys
+
+
+url_OK = ''
+url_NG = ''
+
+
+
+
+##############################################################################################
+# 逆引き
+def reverse_lookup(ip):
+   try:
+      return socket.gethostbyaddr(ip)[0]
+   except:
+      return False
+
+
+
+
+if __name__ == "__main__":
+
+   args = sys.argv
+   if 2 > len(args):
+      print('Invalid arguments')
+      sys.exit(1)
+
+   logtext = args[2]
+   logtexts = logtext.split()
+   url = ""
+   color = ""
+   if args[1] == 'Y':
+      url = url_OK
+      color = "#2EB886"
+      hostname = logtexts[3]
+      username = logtexts[8]
+      ipaddr = logtexts[10]
+      port = logtexts[12]
+   elif args[1] == 'N1':
+      url = url_NG
+      color = "#A30100"
+      hostname = logtexts[3]
+      username = logtexts[10]
+      ipaddr = logtexts[12]
+      port = logtexts[14]
+   else:
+      url = url_NG
+      color = "#A30100"
+      hostname = logtexts[3]
+      username = logtexts[8]
+      ipaddr = logtexts[10]
+      port = logtexts[12]
+
+
+
+
+
+   data = {
+	"attachments": [
+		{
+        "fallback": "System is received an SSH connection at " + hostname + ".",
+        "color": color,
+			"blocks": [
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "System is received an SSH connection at " + hostname + "."
+					}
+				},
+				{
+					"type": "divider"
+				},
+				{
+					"type": "section",
+					"fields": [
+						{
+							"type": "mrkdwn",
+							"text": "*Remote IP Address*\n`" + ipaddr + "`"
+						},
+    						{
+    							"type": "mrkdwn",
+    							"text": "*Remote hostname*\n`" + str(reverse_lookup(ipaddr)) + "`"
+    						},
+						{
+							"type": "mrkdwn",
+							"text": "*Remote Port*\n`" + port + "`"
+						},
+						{
+							"type": "mrkdwn",
+							"text": "*Username*\n`" + username + "`"
+						}
+					]
+				}
+			]
+		}
+	]
+   }
+
+   headers = {
+   'Content-Type': 'application/json',
+   }
+   req = urllib.request.Request(url, json.dumps(data).encode(), headers)
+   with urllib.request.urlopen(req) as res:
+      body = res.read()
+      print(body)
