dc99d2a9f76de9c069d50116abc566f598ecfe7f
shinonome11a <k.ikegami@cosylab.is.oit.ac.jp>
2020-12-15 01:48:43 +0900

    add chmod

--- initial.sh
+++ initial.sh
@@ -5,5 +5,7 @@ echo '% sudo mkdir /etc/swatch'
 sudo mkdir /etc/swatch
 echo '% sudo cp -R ./* /etc/swatch'
 sudo cp -R ./* /etc/swatch
+echo '% sudo chmod -R +x /etc/swatch'
+sudo chmod -R +x /etc/swatch
 echo "% echo '@reboot root /usr/bin/swatchdog -c /etc/swatch/conf.d/auth.log.conf -t /var/log/auth.log > /dev/null' | sudo tee /etc/cron.d/swatch"
 echo '@reboot root /usr/bin/swatchdog -c /etc/swatch/conf.d/auth.log.conf -t /var/log/auth.log > /dev/null' | sudo tee /etc/cron.d/swatch
