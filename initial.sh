3e5fc00a6fce3f7db989f057464b3b79b19882d4
k.ikegami <39215628+shinonome11a@users.noreply.github.com>
2022-08-20 18:12:46 +0900

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+
+# install swatch
 echo '% sudo apt install swatch'
-sudo apt install swatch
+sudo apt update && sudo apt install swatch
+
+# copy original script and swatch config
 echo '% sudo mkdir /etc/swatch'
 sudo mkdir /etc/swatch
 echo '% sudo cp -R ./* /etc/swatch'
 sudo cp -R ./* /etc/swatch
 echo '% sudo chmod -R +x /etc/swatch'
 sudo chmod -R +x /etc/swatch
+
+# autostart setting to cron
 echo "% echo '@reboot root /usr/bin/swatchdog -c /etc/swatch/conf.d/auth.log.conf -t /var/log/auth.log > /dev/null' | sudo tee /etc/cron.d/swatch"
 echo '@reboot root /usr/bin/swatchdog -c /etc/swatch/conf.d/auth.log.conf -t /var/log/auth.log > /dev/null' | sudo tee /etc/cron.d/swatch
